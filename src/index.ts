23c171890215f37271d586ffcdf207d1618d59b7
Michal Stepien <michalstepien@MacBook-Pro-Michal.local>
2021-02-04 12:22:32 +0100

    simple solution - final

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import { router } from './routes/loginRoutes';
+import cookieSession from 'cookie-session';
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ['osaidhfosdihfosdi'] }));
+app.use(router);
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
